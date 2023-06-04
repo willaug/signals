8365342d33b7f52031ad7227f9c1c33ddf0b6738
willaug <william.santos315@outlook.com>
2023-06-04 17:43:30 -0300

    feat: basic concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, effect } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'signals';
+  public counter = signal(0)
+  public lastUpdatedAt: Date | null
+  public lastUpdatedMessage: string | null
+  public readonly tripleCounter = computed(() => Math.pow(this.counter(), 3))
+
+  constructor() {
+    this.lastUpdatedAt = null
+    this.lastUpdatedMessage = null
+
+    effect(() => {
+      this.lastUpdatedAt = new Date()
+      this.lastUpdatedMessage = `The counter has been changed to "${this.counter()}"`
+    });
+  }
+
+  public updateCounter(newValue?: string): void {
+    this.counter.set(Number(newValue))
+  }
 }
